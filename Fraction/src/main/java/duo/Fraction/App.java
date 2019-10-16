f7421ea8fedca3c9bec1c62fc315ffbea46068b8
Utilisateur <user@debian.arcahe.ovh>
2019-10-16 14:21:09 +0200

    first commit

---
+++ Fraction/src/main/java/duo/Fraction/App.java
@@ -0,0 +1,13 @@
+package duo.Fraction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

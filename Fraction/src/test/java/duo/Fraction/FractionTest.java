e4e7f53f83cc654f93e5ebc66a04775474fe117a
gnouf1 <jeuxforum26@gmail.com>
2019-10-16 15:47:04 +0200

    test initI fait

--- Fraction/src/test/java/duo/Fraction/FractionTest.java
+++ Fraction/src/test/java/duo/Fraction/FractionTest.java
@@ -1,17 +1,28 @@
 package duo.Fraction;
 
 import org.junit.*;
+import static org.junit.Assert.* ;
 
-public class FractionTest 
+
+public class FractionTest
 {
-	
-   @Test
+
+  //Test de fonctionnement normal
+  @Test
   public void TestInitI(){
+    Fraction testFrac = new Fraction(1, 1);
+    assertNotNull("L'objet n'existe pas \n", testFrac);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void TestInitI2(){
+    Fraction testFrac = new Fraction(1, 0);
 
   }
-  
+
+
 	@Test
 	public void testInit2() {
-		
+
 	}
 }
